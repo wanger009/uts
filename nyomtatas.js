948c0eba6ff26e416df935cc9eedd4c66fce12b6
wanger009 <75311236+wanger009@users.noreply.github.com>
2021-06-01 00:33:15 +0200

    first commit

---
+++ nyomtatas.js
@@ -0,0 +1,13 @@
+function kalkulal(){
+    //Űrlapadatok
+    const szelesseg=297;
+    const magassag=420;
+    const papir=document.getElementById('papirtipus').value;
+    //Számítások
+    let terulet=Math.round((szelesseg*magassag)/10000);    
+    let valasz=terulet*papir;
+    //Megjelenítés
+    document.getElementById('valasz').style.visibility = "visible";
+}
+
+
